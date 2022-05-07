4cc00325b911224db88ccbdce13e88dfecf67e76
Jijinesh-RJ <jijinesh24@gmail.com>
2022-05-07 11:27:32 +0530

    Modified code

--- src/test/java/com/git/Class1.java
+++ src/test/java/com/git/Class1.java
@@ -1,5 +1,21 @@
 package com.git;
 
 public class Class1 {
+	private void method1() {
+		System.out.println("method1");
+	}
+
+	private void method2() {
+		System.out.println("method2");
+	}
+
+	private void method3() {
+		System.out.println("method3");
+	}
+
+	private void method4() {
+		System.out.println("method4");
+
+	}
 
 }
