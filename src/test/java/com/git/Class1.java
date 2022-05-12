154276568dc98f83fca62e26a6afa65c2f7a8461
Jijinesh-RJ <jijinesh24@gmail.com>
2022-05-12 12:57:45 +0530

    New Code

--- src/test/java/com/git/Class1.java
+++ src/test/java/com/git/Class1.java
@@ -21,5 +21,18 @@ public class Class1 {
 		System.out.println("method5");
 
 	}
+	private void method6() {
+		System.out.println("method6");
+	}
+
+	private void method7() {
+		System.out.println("method7");
+
+	}
+	private void method8() {
+		System.out.println("method8");
+
+	}
+	
 
 }
